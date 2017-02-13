0903e8235654449c3ec75a5e9568c7e0226e67aa
Azaziah-2nd <suutari.olli@gmail.com>
2017-02-13 03:56:59 +0200

    Test commit
    Test commit, improved comment section visuals.

--- Osio5/TravellerQueue.cs
+++ Osio5/TravellerQueue.cs
@@ -8,7 +8,7 @@ namespace Osio5
 {
     /************************************************
     * Olio-ohjelmointi, kevät 2017                  *
-    * Olli Suutari | Opiskelijanro:         *
+    * Olli Suutari | Opiskelijanro:                 *
     * Osio 5, TravellerQueue luokka                 *
     ************************************************/
     public partial class TravellerQueue
