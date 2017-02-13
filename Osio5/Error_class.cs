38cc423d723f32cdb1c79c0fcbcaec7642ae6eda
Azaziah-2nd <suutari.olli@gmail.com>
2017-02-13 09:34:35 +0200

    Virtualized streamreader
    Virtualized streamreader
    Started working on error class (WIP)

---
+++ Osio5/Error_class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osio5
+{
+    class Error_class : Exception
+    {
+        public Error_class(string name)
+        : base(String.Format("Invalid Student Name: {0}", name))
+        {
+
+        }
+    }
+}
--- Osio5/Services.cs
+++ Osio5/Services.cs
@@ -32,22 +32,29 @@ namespace Osio5
         {
             Console.WriteLine("\nPalvelu: {0} | Hinta: {1}\nKuvaus: {2}", Service_name, Service_price, Service_description);
         }
-    }
-
-    public class Room : Services
-    {
-        private DateTime accomodation_starts;
-        private DateTime accomodation_ends;
-        public DateTime Accomodation_starts { get { return accomodation_starts; } set { accomodation_starts = value; } }
-        public DateTime Accomodation_ends { get { return accomodation_ends; } set { accomodation_ends = value; } }
-        private int numPersons;
-        private WirelessInternet internet = null;
-        public WirelessInternet Internet { get { return internet; } set { internet = value; } }
-        public int NumPersons { get { return numPersons; } set { numPersons = value; } }
-
-        public void RoomReadDetails(string filename)
+        public virtual void Read_from_file(string filename)
         {
-            try
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                Service_name = reader.ReadLine();
+                Service_description = reader.ReadLine();
+                Service_price = double.Parse(reader.ReadLine());
+            }
+        }
+    }
+        public class Room : Services
+        {
+            private DateTime accomodation_starts;
+            private DateTime accomodation_ends;
+            public DateTime Accomodation_starts { get { return accomodation_starts; } set { accomodation_starts = value; } }
+            public DateTime Accomodation_ends { get { return accomodation_ends; } set { accomodation_ends = value; } }
+            private int numPersons;
+            private WirelessInternet internet = null;
+            public WirelessInternet Internet { get { return internet; } set { internet = value; } }
+            public int NumPersons { get { return numPersons; } set { numPersons = value; } }
+
+            // Ylikirjoitetaan Read_from_file metodi
+            public override void Read_from_file(string filename)
             {
                 using (StreamReader reader = new StreamReader(filename))
                 {
@@ -57,94 +64,99 @@ namespace Osio5
                     NumPersons = int.Parse(reader.ReadLine());
                 }
             }
-            catch (Exception error)
+            public void Room_service_AskInfo()
             {
-                // Tulostetaan virheilmoitus
-                Console.Write("Tiedostoa ei voitu lukea: " + error.Message);
-                Console.Write("\nYritä uudestaan syöttämällä 'iso' tai pieni: ");
+                WirelessInternet wlan = new WirelessInternet();
+                string str_input;
+                string filename;
+                Console.Write("Luetaanko pienen vai ison mökin tiedot? (pieni/iso): ");
                 filename = Console.ReadLine();
-                // Toistetaan kunnes käyttäjä syöttää oikean tiedostonimen tai peruu syöttämisen.
-                while (filename != "iso" && filename != "pieni")
+                try
                 {
-                    Console.Write("Yritä uudestaan syöttämällä 'iso' tai 'pieni': ");
-                    filename = Console.ReadLine();
+                    Read_from_file(filename + ".txt");
                 }
-                // Luetaan tiedot tiedostosta.
-                StreamReader reader = new StreamReader(filename + ".txt");
-                Service_name = reader.ReadLine();
-                Service_description = reader.ReadLine();
-                Service_price = double.Parse(reader.ReadLine());
-                NumPersons = int.Parse(reader.ReadLine());
-            }
-        }
-        public void Room_service_AskInfo()
-        {
-            WirelessInternet wlan = new WirelessInternet();
-            string str_input;
-            string filename;
-            Console.Write("Luetaanko pienen vai ison mökin tiedot? (pieni/iso): ");
-            filename = Console.ReadLine();
-            RoomReadDetails(filename + ".txt");
+                catch (Exception error)
+                {
+                    // Tulostetaan virheilmoitus
+                    Console.Write("Tiedostoa ei voitu lukea: " + error.Message);
+                    Console.Write("\nYritä uudestaan syöttämällä 'iso' tai pieni: ");
+                    filename = Console.ReadLine();
+                    // Toistetaan kunnes käyttäjä syöttää oikean tiedostonimen tai peruu syöttämisen.
+                    while (filename != "iso" && filename != "pieni")
+                    {
+                        Console.Write("Yritä uudestaan syöttämällä 'iso' tai 'pieni': ");
+                        filename = Console.ReadLine();
+                    }
+                    // Luetaan tiedot tiedostosta.
+                    StreamReader reader = new StreamReader(filename + ".txt");
+                    Service_name = reader.ReadLine();
+                    Service_description = reader.ReadLine();
+                    Service_price = double.Parse(reader.ReadLine());
+                    NumPersons = int.Parse(reader.ReadLine());
+                }
 
-            // Kysytään majoituksen alkamis ja päättymispäivät.
-            Console.Write("Majoituksen aloituspäivämäärä: (pp/kk/vvvv): ");
-            str_input = Console.ReadLine() + " 15:00:00";
-            Accomodation_starts = Convert.ToDateTime(str_input);
-            Console.Write("Majoituksen loppuminen: (pp/kk/vvvv): ");
-            str_input = Console.ReadLine() + " 12:00:00";
-            Accomodation_ends = Convert.ToDateTime(str_input);
-            // Wlan (k/e)
-            Console.Write("Wifi (k/e): ");
-            str_input = Console.ReadLine();
-            while (!str_input.Equals("k") && !str_input.Equals("e"))
-            {
+                // Kysytään majoituksen alkamis ja päättymispäivät.
+                Console.Write("Majoituksen aloituspäivämäärä: (pp/kk/vvvv): ");
+                str_input = Console.ReadLine() + " 15:00:00";
+                Accomodation_starts = Convert.ToDateTime(str_input);
+                Console.Write("Majoituksen loppuminen: (pp/kk/vvvv): ");
+                str_input = Console.ReadLine() + " 12:00:00";
+                Accomodation_ends = Convert.ToDateTime(str_input);
+                // Wlan (k/e)
                 Console.Write("Wifi (k/e): ");
                 str_input = Console.ReadLine();
-            }
-            if (str_input.Equals("k"))
-            {
-                wlan.WlanReadDetails();
-            }
-            // Tulostetaan huoneen yhteenveto.
-            base.Service_base_details();
-            Console.WriteLine("Huoneessa voi majoittua " + NumPersons + " ihmistä.");
-            if (wlan != null)
-            {
-                wlan.Internet_Details();
+                while (!str_input.Equals("k") && !str_input.Equals("e"))
+                {
+                    Console.Write("Wifi (k/e): ");
+                    str_input = Console.ReadLine();
+                }
+                if (str_input.Equals("k"))
+                {
+                    // Luetaan wlanin tiedot tiedostosta wlan.txt
+                    wlan.Read_from_file("wlan.txt");
+                }
+                // Tulostetaan huoneen yhteenveto.
+                base.Service_base_details();
+                Console.WriteLine("Huoneessa voi majoittua " + NumPersons + " ihmistä.");
+                if (wlan != null)
+                {
+                    wlan.Internet_Details();
+                }
             }
         }
-    }
-    public class WirelessInternet : Services
-    {
-        private int bandwidth;
+        public class WirelessInternet : Services
+        {
+            private int bandwidth;
 
-        public int Bandwidth
-        { get { return bandwidth; } set { bandwidth = value; } }
-        private string password;
-        public string Password { get { return password; } set { password = value; } }
-        public void Internet_Details()
-        {
-            base.Service_base_details();
-            Console.WriteLine("Yhteyden nopeus on " + Bandwidth + ", salasana: \"" + Password);
+            public int Bandwidth
+            { get { return bandwidth; } set { bandwidth = value; } }
+            private string password;
+            public string Password { get { return password; } set { password = value; } }
+            public void Internet_Details()
+            {
+                base.Service_base_details();
+                Console.WriteLine("Yhteyden nopeus on " + Bandwidth + ", salasana: \"" + Password);
+            }
+            public override void Read_from_file(string filename)
+            {
+                using (StreamReader reader = new StreamReader(filename))
+                {
+                    Service_name = reader.ReadLine();
+                    Service_description = reader.ReadLine();
+                    Service_price = double.Parse(reader.ReadLine());
+                    Bandwidth = int.Parse(reader.ReadLine());
+                    Password = reader.ReadLine();
+                }
+            }
         }
-        public void WlanReadDetails()
+        public class DogSafari : Services
         {
-            StreamReader reader = new StreamReader("wlan.txt");
-            Service_name = reader.ReadLine();
-            Service_description = reader.ReadLine();
-            Service_price = double.Parse(reader.ReadLine());
-            Bandwidth = int.Parse(reader.ReadLine());
-            Password = reader.ReadLine();
+            private int numDogs;
+            public int NumDogs { get { return numDogs; } set { numDogs = value; } }
+            public void DogSafariDetails()
+            {
+                base.Service_base_details();
+                Console.WriteLine(" Siihen kuuluu " + numDogs + " koiraa. ");
+            }
         }
-    }
-    public class DogSafari : Services
-    {
-        private int numDogs;
-        public int NumDogs { get { return numDogs; } set { numDogs = value; } }
-        public void DogSafariDetails()
-        {
-            base.Service_base_details();
-            Console.WriteLine(" Siihen kuuluu " + numDogs + " koiraa. ");
-        }
-    }
 }
